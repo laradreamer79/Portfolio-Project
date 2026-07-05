34ca52ba41bb3a8a3c235614c894561e8a026266
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-05 21:24:02 +0300

    Add '/me' route with authentication

--- stage4/backend/src/auth/auth.routes.ts
+++ stage4/backend/src/auth/auth.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { login, register } from "./auth.controller.js";
+import { login, me, register } from "./auth.controller.js";
+import { authenticate } from "../middleware/auth.middleware.js";
 
 export const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+
+authRouter.get("/me", authenticate, me);
