6a7eac99f9056239904feef5b0ab38e7fb9e3a06
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 18:50:48 +0300

    Add BookingsModule with controller and services

--- stage4/backend/bookings/bookings.module.ts
+++ stage4/backend/bookings/bookings.module.ts
@@ -1,1 +1,23 @@
-export {};
+import { Module } from '@nestjs/common';
+
+import { BookingsController } from './bookings.controller';
+import { BookingsService } from './bookings.service';
+
+import { PrismaService } from '../database/prisma.service';
+
+
+@Module({
+  controllers: [
+    BookingsController,
+  ],
+
+  providers: [
+    BookingsService,
+    PrismaService,
+  ],
+
+  exports:[
+    BookingsService,
+  ],
+})
+export class BookingsModule {}
