acd60a7987ff152b2125a240129bada173aff62b
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 19:08:42 +0300

    Create ReviewEntity class for reviews module

---
+++ stage4/backend/src/reviews/entities/review.entity.ts
@@ -0,0 +1,19 @@
+export class ReviewEntity {
+  id: number;
+
+  rating: number;
+
+  comment?: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  userId: number;
+
+  centerId: number;
+
+  tripId?: number;
+
+  courseId?: number;
+}
