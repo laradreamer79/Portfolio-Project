6b7d0d0cb5ce4340026565a4492e12bb005a7164
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 17:38:32 +0300

    Implement booking service functions
    Added functions for booking management and availability checks.

--- stage4/backend/bookings/bookings.service.ts
+++ stage4/backend/bookings/bookings.service.ts
@@ -1,1 +1,17 @@
+import { prisma } from "../config/prisma.js";
 
+export async function createBooking() {
+  // TODO
+}
+
+export async function cancelBooking() {
+  // TODO
+}
+
+export async function getUserBookings() {
+  // TODO
+}
+
+export async function checkAvailability() {
+  // TODO
+}
