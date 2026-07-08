741163c327553cca2203f1cf72f975fccdcdb928
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 19:07:40 +0300

    Add BookingEntity class with relevant properties

--- stage4/backend/src/bookings/entities/booking.entity.ts
+++ stage4/backend/src/bookings/entities/booking.entity.ts
@@ -1,1 +1,19 @@
+export class BookingEntity {
+  id: number;
 
+  numberOfPeople: number;
+
+  totalPrice: number;
+
+  status: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  userId: number;
+
+  tripId?: number;
+
+  courseId?: number;
+}
