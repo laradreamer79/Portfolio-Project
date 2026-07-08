8f20c4d17585245ac25967c170a483c09ba74c2b
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 18:51:51 +0300

    Add ReviewsModule with controller and services

--- stage4/backend/reviews/reviews.module.ts
+++ stage4/backend/reviews/reviews.module.ts
@@ -1,1 +1,28 @@
-export {};
+import { Module } from '@nestjs/common';
+
+import { ReviewsController } from './reviews.controller';
+import { ReviewsService } from './reviews.service';
+
+import { PrismaService } from '../database/prisma.service';
+
+
+
+@Module({
+
+  controllers:[
+    ReviewsController,
+  ],
+
+
+  providers:[
+    ReviewsService,
+    PrismaService,
+  ],
+
+
+  exports:[
+    ReviewsService,
+  ],
+
+})
+export class ReviewsModule {}
