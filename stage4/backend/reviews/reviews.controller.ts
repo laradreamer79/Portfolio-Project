84314792155d3991abea2415f2927d6cb33765ff
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 18:40:30 +0300

    Implement ReviewsController with create and findAll methods

--- stage4/backend/reviews/reviews.controller.ts
+++ stage4/backend/reviews/reviews.controller.ts
@@ -1,1 +1,46 @@
+import {
+ Controller,
+ Post,
+ Get,
+ Body,
+ Req,
+} from '@nestjs/common';
 
+import { ReviewsService } from './reviews.service';
+
+
+@Controller('reviews')
+export class ReviewsController {
+
+
+constructor(
+ private reviewsService:ReviewsService
+){}
+
+
+
+@Post()
+create(
+ @Req() req,
+ @Body() body
+){
+
+return this.reviewsService.create(
+ req.user.id,
+ body,
+);
+
+}
+
+
+
+@Get()
+findAll(){
+
+return this.reviewsService.findAll();
+
+}
+
+
+
+}
