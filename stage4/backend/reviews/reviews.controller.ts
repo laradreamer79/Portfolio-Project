8be5526866ab2c2d4f2a21f7c5cd6dcee6b8a3ee
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-08 18:51:36 +0300

    Refactor ReviewsController methods and structure

--- stage4/backend/reviews/reviews.controller.ts
+++ stage4/backend/reviews/reviews.controller.ts
@@ -1,46 +1,67 @@
 import {
- Controller,
- Post,
- Get,
- Body,
- Req,
+  Controller,
+  Post,
+  Get,
+  Body,
+  Req,
+  Param,
 } from '@nestjs/common';
 
+
 import { ReviewsService } from './reviews.service';
+
 
 
 @Controller('reviews')
 export class ReviewsController {
 
 
-constructor(
- private reviewsService:ReviewsService
-){}
+  constructor(
+    private readonly reviewsService: ReviewsService,
+  ) {}
 
 
 
-@Post()
-create(
- @Req() req,
- @Body() body
-){
 
-return this.reviewsService.create(
- req.user.id,
- body,
-);
+  // Create review
+  @Post()
+  async createReview(
+    @Req() req,
+    @Body() body,
+  ){
 
-}
+    return this.reviewsService.createReview(
+      req.user.id,
+      body,
+    );
+
+  }
 
 
 
-@Get()
-findAll(){
 
-return this.reviewsService.findAll();
+  // Get all reviews
+  @Get()
+  async getReviews(){
 
-}
+    return this.reviewsService.getReviews();
 
+  }
+
+
+
+
+  // Get reviews by center
+  @Get('center/:id')
+  async getCenterReviews(
+    @Param('id') id:string,
+  ){
+
+    return this.reviewsService.getCenterReviews(
+      Number(id),
+    );
+
+  }
 
 
 }
